c39b97b39a0fc0f264d5e5ea2d301ac61960165e
Ekiww <harisshidqi99@gmail.com>
2025-07-10 20:59:07 +0700

    update

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,15 @@
 <?php
 
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
+
+$host = 'sql100.infinityfree.com'; // ganti XXX sesuai info dari cpanel
+$user = 'if0_39438926'; // username dari cpanel
+$pass = 'bdG9qCU9oyGLA'; // password akun database
+$db   = 'if0_39438926_kelompok_3';
+$koneksi = mysqli_connect($host, $user, $pass ,$db);
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+} else {
+    echo "Koneksi berhasil!";
 }
-
-$koneksi = mysqli_connect('localhost', 'root', '', 'kelompok_3');
-
-
 ?>
